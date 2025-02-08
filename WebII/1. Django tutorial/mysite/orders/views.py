4860e22878f513bbb3920e2b7e73c11d02862243
BeaRozz <asucamedina@gmail.com>
2025-02-07 20:42:01 -0600

    Clase 07-feb-2025

--- WebII/1. Django tutorial/mysite/orders/views.py
+++ WebII/1. Django tutorial/mysite/orders/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Question
 
 
 def indexOrders(request):
-    return HttpResponse("Hello, world. You're at the orders index.")
+    data = {
+        "questions": Question.objects.all(),
+        "titulo":"Index de orders",
+        "total_orders":100,
+        "total_payments":200,
+        "orders":[
+            {
+                "id":1, "total":100
+            },
+            {
+                "id":2, "total":200
+            },
+            {
+                "id":3, "total":300
+            },
+            {
+                "id":4, "total":400
+            }
+        ]
+    }
+    return render(request, 'orders/index.html', data)
+
 
 # Create your views here.
