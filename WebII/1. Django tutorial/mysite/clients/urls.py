7ba94d7c6bd334db0d16bd09717091c13d67b58a
BeaRozz <asucamedina@gmail.com>
2025-02-21 11:02:44 -0600

    Práctica 5

--- WebII/1. Django tutorial/mysite/clients/urls.py
+++ WebII/1. Django tutorial/mysite/clients/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("create", views.createUserView, name="createUserView"),
     path("createUser", views.createUser, name="createUser"),
     path("createUser-by-fetch", views.createUserByFetch, name="createUserByFetch"),
-    path("details/<int:id>", views.userDetail, name="UserDetail")
+    path("details/<int:id>", views.userDetail, name="UserDetail"),
+    path("update/<int:id>", views.updateUserView, name="updateUserView"),
+    path('update_user/<int:id>/', views.updateUser, name='update_user')
 ]
--- WebII/1. Django tutorial/mysite/clients/views.py
+++ WebII/1. Django tutorial/mysite/clients/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import User
+from .models import User_address
 from django.utils import timezone
 from django.http import JsonResponse
 import json
@@ -51,3 +52,45 @@ def userDetail(request, id):
     user = get_object_or_404(User, id=id)
     # user = User.objects.get(id=id) marca error feo y todo, el de arriba tiene coomo un trycatch
     return render(request, "clients/detail.html", {"user": user})
+
+def updateUserView(request, id):
+    user = get_object_or_404(User, id=id)
+    user_address = user.user_address_set.all()
+
+    data = {
+        "user": user,
+        "address": user_address
+    }
+    return render(request, "clients/update.html", data)
+
+def updateUser(request, id):
+    data = {}
+    user = get_object_or_404(User, id=id)
+
+    try:
+        if request.method == "POST":
+            name = request.POST.get("name")
+            email = request.POST.get("email")
+            age = request.POST.get("age")
+            rfc = request.POST.get("rfc")
+            photo = request.POST.get("photo")
+            updated_date = timezone.now()
+
+            user.name = name
+            user.email = email
+            user.age = age
+            user.rfc = rfc
+            user.photo = photo
+            user.updated_date = updated_date
+            user.save()    
+            
+            data["user"] = user
+            data["message"] = "Edit the user data"
+            data["status"] = "info"
+            data["address"] = user.user_address_set.all()
+    
+    except Exception as e:
+        data["message"] = str(e)
+        data["status"] = "error"
+
+    return render(request, "clients/update.html", data)
