db39d80c74d0748c78571261e3351332863704cc
BeaRozz <asucamedina@gmail.com>
2025-03-13 19:14:19 -0600

    Delete Django files

--- WebII/1. Django tutorial/mysite/examen/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Localidad, Producto, Evento, Boleto
-
-admin.site.register(Localidad)
-admin.site.register(Producto)
-admin.site.register(Evento)
-admin.site.register(Boleto)
